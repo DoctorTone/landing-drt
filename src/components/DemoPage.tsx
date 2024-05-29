ab6128c1c715d519928a5343984f4387b30fb55d
DoctorTone <tony@drt-software.com>
2024-05-29 13:53:49 +0100

    Update demo page paper

--- src/components/DemoPage.tsx
+++ src/components/DemoPage.tsx
@@ -1,25 +1,43 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 import { Container } from "@mui/material";
 import { Typography } from "@mui/material";
 
 const DemoPage = () => {
   return (
-    <Box sx={{ mt: 10, backgroundColor: "grey" }}>
+    <Box sx={{ mt: 10, pb: 10, backgroundColor: "grey" }}>
       <Container>
-        <Typography textAlign="center" variant="h3">
-          See some of our demos
+        <Typography sx={{ paddingTop: 3 }} textAlign="center" variant="h3">
+          Demos
         </Typography>
-        <Grid container>
+        <Grid container sx={{ mb: 5 }} spacing={30}>
           <Grid item xs={6}>
-            <Typography>Unleash the power of 3D.</Typography>
+            <Paper sx={{ textAlign: "center" }} elevation={3}>
+              <Typography>
+                One of the most common uses of 3D is for product configurators.
+              </Typography>
+            </Paper>
           </Grid>
           <Grid item xs={6}>
-            <Typography>
-              More engaging interaction, enhanced user experiences and increased
-              sales for your website.
-            </Typography>
+            <Paper elevation={3}>
+              This demo shows how something can be configured
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid container spacing={30}>
+          <Grid item xs={6}>
+            <Paper sx={{ textAlign: "center" }} elevation={3}>
+              <Typography>
+                One of the most common uses of 3D is for product configurators.
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper elevation={3}>
+              This demo shows how something can be configured
+            </Paper>
           </Grid>
         </Grid>
       </Container>
