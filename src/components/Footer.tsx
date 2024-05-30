7c53f0bd0eb09c7fc07833b09f1f772a75f33ce6
DoctorTone <tony@drt-software.com>
2024-05-31 00:41:14 +0100

    Added footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,33 @@ import Box from "@mui/material/Box";
 import { Container } from "@mui/material";
 import { Typography } from "@mui/material";
 import CopyrightIcon from "@mui/icons-material/Copyright";
+import Stack from "@mui/material/Stack";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const Footer = () => {
   return (
     <Box sx={{ pt: 3, pb: 5, backgroundColor: "#222222", color: "orange" }}>
       <Container>
-        <Typography>
-          <CopyrightIcon /> DRT Software Ltd 2024
-        </Typography>
+        <Stack alignItems="center" sx={{ mb: 2 }}>
+          <LocationOnIcon fontSize="large" color="warning" />
+        </Stack>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CopyrightIcon sx={{ mr: 1 }} />
+          <Typography>DRT Software Ltd 2024</Typography>
+          <MoreVertIcon />
+          <Typography>1 Highfield Drive, Nuthall, Nottingham</Typography>
+          <MoreVertIcon />
+          <Typography>Company No: 09710237</Typography>
+          <MoreVertIcon />
+          <Typography>VAT No: 331 7022 43</Typography>
+        </Box>
       </Container>
     </Box>
   );
